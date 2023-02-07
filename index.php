d7026f1c0633c26c3543b26b0cdfd441cdc1d7ee
SumNic <108144938+SumNic@users.noreply.github.com>
2023-02-07 11:09:05 +0300

    Create index.php
    sdf

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form name="button" id="button">
+    <input type="file" name="file">
+    <input type="reset" onclick="fileLoad()">
+</form>   
+<script>
+    function sendMessage() {
+  
+  var formData = new FormData(document.forms.button);
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    const respons = this.responseText;
+    if (respons !== ""){
+      div_message.style = "display:flex";
+      message_message.innerHTML = respons;
+      setTimeout(function() {
+        div_message.style = "display:none";
+      }, 2000);
+    } else {
+      // открывается отправленное сообщение
+      setTimeout(openNewMesseg, 1000);
+      // openNewMesseg();
+      // передвигает страницу к последему сообщению
+      setTimeout(reLoad, 2000);
+    }
+  }
+  document.getElementById("send_message").reset();
+  xhttp.open("POST", "button.php", true);
+  // xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhttp.send(formData);
+}
+</script> 
+</body>
+</html>
